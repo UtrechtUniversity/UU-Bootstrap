c3e8ab7627b363dd1c6fd3b02720ea447f800e8d
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2022-07-25 14:14:13 +0200

    Changed tiles-n breakpoint class behaviour

---
+++ docs/pages/components/tiles.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const range = ref([...Array(56).keys()]);
+</script>
+<template>
+    <div class="uu-content">
+        <Head>
+            <Title>Tiles | Portaldev Bootstrap theme docs</Title>
+        </Head>
+        <div class="uu-hero">
+            <h1>Tiles</h1>
+        </div>
+        <div class="uu-container flex-nowrap">
+            <div class="tiles">
+                <a class="tile" v-for="i in range" :key="i">
+                    <strong class="mb-auto">Tile {{i}}</strong>
+                    <span>Something </span>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
